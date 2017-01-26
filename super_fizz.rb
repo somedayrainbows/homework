0ff26c7c4c4654c2ba18481f623978d07bca5ea3
somedayrainbows <ebassity@gmail.com>
2017-01-25 21:50:38 -0700

    SuperFizz homework

--- super_fizz.rb
+++ super_fizz.rb
@@ -6,13 +6,13 @@
 
 
 (1..1000).each do |number|
-  if number % 3 == 0 and number % 5 == 0 and number % 7 == 0
+  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0
         puts "SuperFizzBuzz"
-  elsif number % 3 == 0 and number % 7 == 0
+  elsif number % 3 == 0 && number % 7 == 0
         puts "SuperFizz"
-  elsif number % 5 == 0 and number % 7 == 0
+  elsif number % 5 == 0 && number % 7 == 0
         puts "SuperBuzz"
-  elsif number % 3 == 0 and number % 5 == 0
+  elsif number % 3 == 0 && number % 5 == 0
         puts "FizzBuzz"
   elsif number % 3 == 0
         puts "Fizz"
